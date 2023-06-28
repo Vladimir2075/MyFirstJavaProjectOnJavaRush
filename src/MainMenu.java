8a9b98712e36f286e271cd2a5f3d891d611570ce
Vladimir2075 <110116510+Vladimir2075@users.noreply.github.com>
2023-06-28 11:17:22 +0300

    implemented MainMenu

---
+++ src/MainMenu.java
@@ -0,0 +1,13 @@
+public class MainMenu {
+    public MainMenu() {
+        System.out.println("Головне меню:");
+        RunMainMenu(0);
+    }
+
+    // level -Menu level
+    public void RunMainMenu (int level){
+
+    }
+
+
+}
