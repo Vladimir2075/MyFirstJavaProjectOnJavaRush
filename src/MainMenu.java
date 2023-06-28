7c72fe6d55f6ccbee02d499c8af97a946ad7cff9
Vladimir2075 <110116510+Vladimir2075@users.noreply.github.com>
2023-06-28 11:20:40 +0300

    implemented drawing menu

--- src/MainMenu.java
+++ src/MainMenu.java
@@ -1,4 +1,11 @@
 public class MainMenu {
+    final String NAME_FIRST_OPTION = "1.Шифр Цезаря";
+    final String NAME_FIRST_FIRST_OPTION = "1.Шифрування (для вибору введіть 1 ";
+    final String NAME_FIRST_SECOND_OPTION = "2.Дешифрування (для вибору введіть 2)";
+    final String NAME_FIRST_THIRD_OPTION = "3.Перехід в основне меню(для вибору введіть 3)";
+    final String NAME_SECOND_OPTION = "2.Криптоаналіз методом brute force";
+    final String YOUR_CHOICE = "Ваш вибір:";
+    final String NAME_OPTION_EXIT = "3.Вихід (3)";
     public MainMenu() {
         System.out.println("Головне меню:");
         RunMainMenu(0);
@@ -9,5 +16,11 @@ public class MainMenu {
 
     }
 
+    private void menuDrawing (String arg1, String arg2, String arg3){
+        System.out.println(arg1);
+        System.out.println(arg2);
+        System.out.println(arg3);
+        System.out.print(YOUR_CHOICE);
+    }
 
 }
